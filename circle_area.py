d8b1b187199c93b3cb4b775707fdbaee1c3d5b1d
marco <marco.gr@pachuca.tecnm.mx>
2024-08-06 13:06:51 -0600

    initial commit

---
+++ circle_area.py
@@ -0,0 +1,14 @@
+# ******************
+# ÁREA DE UN CÍRCULO
+# ******************
+
+
+def run(radius: float) -> float:
+    # TU CÓDIGO AQUÍ
+    area = 'output'
+
+    return area
+
+
+if __name__ == '__main__':
+    run(4)
